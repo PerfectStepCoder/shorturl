ea86c11571ac6553a0b14c5495aab30ff90aa2df
CreatorDmitrij <codisculptor@gmail.com>
2024-07-17 08:38:07 +0300

    Add sync

--- internal/storage/inmemory.go
+++ internal/storage/inmemory.go
@@ -3,9 +3,11 @@ package storage
 import (
 	"crypto/sha256"
 	"encoding/hex"
+	"sync"
 )
 
 type StorageInMemory struct {
+	mu sync.Mutex // синхронизация доступа к хранилищу
 	data           map[string]string
 	lengthShortURL int
 }
@@ -15,6 +17,8 @@ func NewStorage(lengthShortURL int) *StorageInMemory {
 }
 
 func (s *StorageInMemory) Save(value string) string {
+	s.mu.Lock()
+	defer s.mu.Unlock()
 	hash := sha256.New()
 	hash.Write([]byte(value))
 	hashKey := hex.EncodeToString(hash.Sum(nil))
@@ -24,6 +28,8 @@ func (s *StorageInMemory) Save(value string) string {
 }
 
 func (s *StorageInMemory) Get(hashKey string) (string, bool) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
 	value, exists := s.data[hashKey]
 	return value, exists
 }
