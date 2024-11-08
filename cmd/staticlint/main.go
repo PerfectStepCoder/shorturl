3463047a3b090a145d7b705aa4e551cb9e89730e
CreatorDmitrij <codisculptor@gmail.com>
2024-11-08 12:26:39 +0100

    Up

--- cmd/staticlint/main.go
+++ cmd/staticlint/main.go
@@ -61,8 +61,13 @@ func main() {
 
 	// Включаем все анализаторы класса SA из staticcheck
 	for _, v := range staticcheck.Analyzers {
-		if v.Analyzer.Name[:2] == "SA" {
-			mychecks = append(mychecks, v.Analyzer)
+		switch v.Analyzer.Name[:2] {
+			case "SA": // анализаторы класса SA
+				mychecks = append(mychecks, v.Analyzer)
+			case "S": // добавляем по крайней мере один анализатор из класса S
+				mychecks = append(mychecks, v.Analyzer)
+			case "QF": // добавляем по крайней мере один анализатор из класса QF
+				mychecks = append(mychecks, v.Analyzer)
 		}
 	}
 
