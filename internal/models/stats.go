6051edf783339a0c95b387e16797da980484e7a4
CreatorDmitrij <codisculptor@gmail.com>
2024-11-27 16:11:37 +0100

    Fix

--- internal/models/stats.go
+++ internal/models/stats.go
@@ -1,6 +1,7 @@
 // Модуль models содержит описание получаемых и возвращаемых сущностей HTTP сервисом.
 package models
 
+// ResponseStatsBase - возвращаемый результат обработки статситики обработанных ссылок пользователями.
 type ResponseStatsBase struct {
 	Urls  int `json:"urls"`  // количество сокращённых URL в сервисе
 	Users int `json:"users"` // количество пользователей в сервисе
